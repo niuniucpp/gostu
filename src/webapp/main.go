93fe20db4da64f6b536521dccc25953cd15b1440
niuniucpp <17748408109@163.com>
2024-05-21 11:14:46 +0800

    update README.MD

--- src/webapp/main.go
+++ src/webapp/main.go
@@ -12,8 +12,12 @@ func header(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", header)
 	fmt.Printf("正在运行中……")
+	//getLogin("zhangshang", 1)
 	//创建路由
 	http.ListenAndServe("localhost:8080", nil)
 	fmt.Printf("程序结束")
+	//http.HandleFunc("/golang", header)
+	//http.ListenAndServe("localhost:8080/golang", nil)
+	//fmt.Printf("/golang,程序启动成功")
 
 }
---
+++ src/webapp/work.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func getLogin(name string, num int) {
+	fmt.Println(name)
+	fmt.Println(num)
+}
